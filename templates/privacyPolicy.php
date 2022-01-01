de2692636e3f0f0230ce36c0193360fae1e6950c
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:56:23 +0100

    Create privacyPolicy.php

---
+++ templates/privacyPolicy.php
@@ -0,0 +1,17 @@
+<?php require_once __DIR__.'/header.php'?>
+<div class="container">
+<h2>Privacy Policy</h2>
+<h3>Your Guide to All Things Privacy</h3>
+<h4>1. All for you</h4>
+<p>Your data will only be used to benefit you and your shopping experience.</p>
+<h4>2. Our promise</h4>
+<p>You have our word that we’ll treat and protect your data like it’s our own.</p>
+<h4>3. Straight up</h4>
+<p>We’ll give it to you straight and keep you updated with what you need to know.</p>
+<h4>4. No small talk</h4>
+<p>Let us know your preferences - you decide what and how you hear from us.</p>
+<h4>5. Just the essentials</h4>
+<p>We won’t keep your data for no reason. If we don’t need them, we’ll delete them</p>
+<p>Protecting your Privacy At Ricardo Milos, we’ve got your back when it comes to your style and those all-important personal details. Our customers are super important to us, which means protecting our customers by keeping their personal data and information secure at all costs is a main priority. We know the world of data security can be tricky (and a little zzzz), but we want you to be fully clued up on everything you need to know when it comes to your personal information and how it’s used. Consider this your guide to all things privacy related.</p>
+</div>
+<?php require_once __DIR__.'/footer.php'?>
