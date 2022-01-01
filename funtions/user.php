86f98b81b874771c3f257416153dd3338bbe5dc1
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:51:42 +0100

    Create user.php

---
+++ funtions/user.php
@@ -0,0 +1,92 @@
+<?php
+function getCurrentUserId():?int
+{
+  $userId = null;
+  if(isset($_SESSION['userId']))
+  {
+    $userId= (int) $_SESSION['userId'];
+  }
+  return $userId;
+}
+function isLoggedIn():bool
+{
+  return isset($_SESSION['userId']);
+}
+function createAccount(string $username, string $password, string $email):bool
+{
+  $password=password_hash($password,PASSWORD_DEFAULT);
+  $sql="INSERT INTO user SET username= :username, password=:password, email=:email";
+  $statement = getDB()->prepare($sql);
+  if(false === $statement)
+  {
+    return false;
+  }
+  $affectedRows = $statement->execute(
+    [
+      ':username'=>$username,
+      ':password'=>$password,
+      ':email'=>$email,
+    ]
+  );
+  return $affectedRows > 0;
+}
+function getUserDataForId(?int $userId):array
+{
+  if(null===$userId)
+  {
+    $userId=getCurrentUserId();
+  }
+  $sql="SELECT id, password,CONCAT_WS('-','KD',SUBSTR(username,1,3),id) AS customerNumber
+   FROM user WHERE id=:id";
+  $statement = getDB()->prepare($sql);
+  if(false === $statement)
+  {
+    return [];
+  }
+  $statement->execute([':id'=>$userId]);
+  if(0 === $statement->rowCount())
+  {
+    return[];
+  }
+  $row =$statement->fetch();
+  return $row;
+}
+function usernameExists(string $username):bool
+{
+  $sql ="SELECT 1 FROM user WHERE username=:username";
+  $statement=getDB()->prepare($sql);
+  if(false==$statement)
+  {
+    return false;
+  }
+  $statement->execute([':username'=>$username]);
+  return (bool)$statement->fetchColumn();
+}
+function emailExists(string $email):bool
+{
+  $sql ="SELECT 1 FROM user WHERE email=:email";
+  $statement=getDB()->prepare($sql);
+  if(false==$statement)
+  {
+    return false;
+  }
+  $statement->execute([':email'=>$email]);
+  return (bool)$statement->fetchColumn();
+}
+function getUserDataForUserName(string $username):array
+{
+$sql="SELECT id, password
+ FROM user WHERE username=:username";
+$statement = getDB()->prepare($sql);
+if(false === $statement)
+{
+  return [];
+}
+$statement->execute([':username'=>$username]);
+if(0 === $statement->rowCount())
+{
+  return[];
+}
+$row =$statement->fetch();
+return $row;
+}
