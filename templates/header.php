4f279dc2ea32d9b84791edcaf98a1b548dda82cf
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:55:09 +0100

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <title>Minh</title>
+  <base href="<?= $baseUrl ?>">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+  <?php include __DIR__.'/navbar.php'?>
+  <header class="jumbotron">
+    <div class="container">
+      <h1>Ricardo Milos Shop</h1>
+    </div>
+  </header>
