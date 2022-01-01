8637caa2c75217efa5e69c17364da4a49f937403
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:54:00 +0100

    Create cartItem.php

---
+++ templates/cartItem.php
@@ -0,0 +1,10 @@
+<div class="col-3">
+<img class="productPicture" src="<?= $cartItem['image']?>">
+</div>
+<div class="col-7">
+  <div><?= $cartItem['titel']?></div>
+  <div><?= $cartItem['describtion']?></div>
+</div>
+<div class="col-2 text-end">
+  <span class="price"><?= $cartItem['price']?> €</div>
+</div>
