400e27d7ee7455ed4cbfd40dcf1be2035ffc8b44
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:57:43 +0100

    Create includes.php

---
+++ includes.php
@@ -0,0 +1,12 @@
+<?php
+require_once CONFIG_DIR.'/companyData.php';
+require_once __DIR__.'/functions/database.php';
+require_once __DIR__.'/functions/cart.php';
+require_once __DIR__.'/functions/user.php';
+require_once __DIR__.'/functions/product.php';
+require_once __DIR__.'/functions/utilities.php';
+require_once __DIR__.'/functions/deliveryAddresses.php';
+require_once __DIR__.'/functions/paypal.php';
+require_once __DIR__.'/functions/orders.php';
+require_once __DIR__.'/routes.php';
+?>
