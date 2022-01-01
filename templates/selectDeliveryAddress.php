9fb1f20939c5365b46f9083460d03d1138d01fc2
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:56:45 +0100

    Create selectDeliveryAddress.php

---
+++ templates/selectDeliveryAddress.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__.'/header.php' ?>
+
+<section class="container" id="selectDeliveryAddress">
+  <?php require_once __DIR__.'/deliveryAddressList.php' ?>
+</section>
+
+<section class="container" id="newDeliveryAddress">
+ <?php require_once __DIR__.'/deliveryAddressForm.php' ?>
+</section>
+
+<?php require_once __DIR__.'/footer.php' ?>
