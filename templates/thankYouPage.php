63fdb486df96f7c88128682f4a73278a927be0d8
songthan9xa1 <76012525+songthan9xa1@users.noreply.github.com>
2022-01-01 22:57:19 +0100

    Create thankYouPage.php

---
+++ templates/thankYouPage.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__."/header.php" ?>
+<section class="container">
+  <strong>Vielen dank für Ihre Einkauf</strong>
+  </section>
+<div class="col-2 offset-1">
+<a class="btn btn-success" href="index.php/invoice/<?=$number?>">Rechnung Anzeigen</a>
+</div>
+<?php require_once __DIR__."/footer.php" ?>
